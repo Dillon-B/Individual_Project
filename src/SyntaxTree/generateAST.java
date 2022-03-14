54dcb1d629bbe9171c18c5f34fe3de7f2e7c4adf
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2022-03-14 17:39:08 +0000

    Update generateAST.java

--- src/SyntaxTree/generateAST.java
+++ src/SyntaxTree/generateAST.java
@@ -6,4 +6,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class generateAST {
+    public static void main (String [] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+    }
 }
