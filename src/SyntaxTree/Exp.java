c3f19e920e2214828645b7dcacb366d34cc1c6e1
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2022-03-14 17:19:26 +0000

    Update Exp.java

--- src/SyntaxTree/Exp.java
+++ src/SyntaxTree/Exp.java
@@ -1,4 +1,11 @@
 package SyntaxTree;
 
+import Compile.Main;
+
 public abstract class Exp {
+
+    @Override
+    public String toString() {
+        return "toString not overridden in class";
+    }
 }
