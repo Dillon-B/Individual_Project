32f9864a29e87749e56de01c8252fce8149acca7
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2022-03-21 10:55:23 +0000

    Update Exp.java

--- src/SyntaxTree/Exp.java
+++ src/SyntaxTree/Exp.java
@@ -1,8 +1,19 @@
 package SyntaxTree;
 
 
-public abstract class Exp {
+import TypeCheck.Token;
 
+public abstract class Exp {
+    static class Op extends Exp {
+        Op(Exp left, Token operator, Exp right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+        final Exp left;
+        final Token operator;
+        final Exp right;
+    }
     @Override
     public String toString() {
         return "toString not overridden in class";
