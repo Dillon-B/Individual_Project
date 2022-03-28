ad4ea16449ebc77d0463e7eca5af6cc5c118b1ac
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2022-03-28 13:25:16 +0100

    Update Exp.java

--- src/SyntaxTree/Exp.java
+++ src/SyntaxTree/Exp.java
@@ -14,6 +14,27 @@ public abstract class Exp {
         final Token operator;
         final Exp right;
     }
+    static class Stm extends Exp {
+        Stm(Exp statement) {
+            this.statement = statement;
+        }
+        final Exp statement;
+    }
+    static class Literal extends Exp {
+        Literal(Object value) {
+            this.value = value;
+        }
+        final Object value;
+    }
+    static class Float extends Exp {
+        Float(Token operator, Exp right) {
+            this.operator = operator;
+            this.right = right;
+        }
+        final Token operator;
+        final Exp right;
+    }
+
     @Override
     public String toString() {
         return "toString not overridden in class";
