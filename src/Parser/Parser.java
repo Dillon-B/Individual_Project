bf8455e405fced720adf1dd991211e346c2271ab
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2022-04-06 14:23:15 +0100

    Update Parser.java

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,7 +1,17 @@
 package Parser;
 
 import java.util.List;
+
+import TypeCheck.Token;
 import TypeCheck.TokenType.*;
 
 public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
 }
