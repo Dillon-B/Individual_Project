cb694b6ca6765ab6391b31477fa68e838664cb1c
Dillon-B <Dillon.Bhanderi@city.ac.uk>
2022-05-13 11:28:18 +0100

    Update Main.java

--- src/Compile/Main.java
+++ src/Compile/Main.java
@@ -20,13 +20,12 @@ public class Main {
     static boolean hasError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
-            System.out.println("Python");
+            System.out.println("Too Many Arguments");
             System.exit(64);
         } else if (args.length == 1) {
             runFile(args[0]);
-        } else {
-            runPrompt();
         }
+
     }
 
     private static void runFile(String path) throws IOException {
@@ -35,27 +34,27 @@ public class Main {
         if (hasError) System.exit(65);
     }
 
-    private static void runPrompt() throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
-
-        for (; ; ) {
-            System.out.print("> ");
-            String line = reader.readLine();
-            if (line == null) break;
-            run(line);
-            hasError = false;
-        }
-    }
+//    private static void runPrompt() throws IOException {
+//        InputStreamReader input = new InputStreamReader(System.in);
+//        BufferedReader reader = new BufferedReader(input);
+//
+//        for (; ; ) {
+//            System.out.print("> ");
+//            String line = reader.readLine();
+//            if (line == null) break;
+//            run(line);
+//            hasError = false;
+//        }
+//    }
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 //
 //        // For now, just print the tokens.
-//        for (Token token : tokens) {
-//            System.out.println(token);
-
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
         Parser parser = new Parser(tokens);
         Exp expression = parser.parse();
 
